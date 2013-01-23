79788a725ebda4399a5bee4b81fc94b27b9b4f5d
abstractOwl <andrewlo@umail.ucsb.edu>
2013-01-22 23:48:55 -0800

    Added messages

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -20,33 +20,43 @@ void HashTable::insert(std::string key, std::string value)
 {
 	int hash	=	str2hash(key);
 
-	// Construct new Entry
-	Entry *newEntry	=	new Entry;
-	newEntry->key	=	key;
-	newEntry->value	=	value;
-	newEntry->hash	=	hash;
-	newEntry->next	=	entries[hash];
+	if (find(key) == NULL) {
+		// Construct new Entry
+		Entry *entry	=	new Entry;
+		entry->key		=	key;
+		entry->value	=	value;
+		entry->hash		=	hash;
+		entry->next		=	entries[hash];
 
-	entries[hash]	=	newEntry; // Add new Entry to front
+		entries[hash]	=	entry; // Add new Entry to front
+		std::cout << "item successfully inserted" << std::endl;
+	} else { // Item already exists
+		std::cout << "item already present" << std::endl;
+	}
 }
 // Removes a specified string from the HashTable
 void HashTable::remove(std::string key)
 {
+	bool	found	=	false;
 	int		hash	=	str2hash(key);
 	Entry*	curr = entries[hash];
 
 	// Remove Entry containing key from array
 	if (curr->key == key) { // Key is first
 		entries[hash] = curr->next;
+		found = true;
 	} else {
 		while (curr != NULL) { // Search for key
 			if (curr->next->key == key) {
 				curr->next = curr->next->next; // previousKey->next => nextKey
+				found = true;
 				break;
 			}
 			curr = curr->next;
 		}
 	}
+	std::cout << "item " << (found ? "successfully deleted" :
+		"not present in the table") << std::endl;
 }
 // Returns a reference to the instance of specified key in the HashTable if it
 // exists.
@@ -60,6 +70,7 @@ std::string* HashTable::find(std::string key)
 	// Search for key
 	while (curr != NULL) {
 		if (curr->key == key) {
+			std::cout << "item found" << std::endl;
 			return &curr->value;
 		}
 		curr = curr->next;
