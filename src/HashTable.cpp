9ec023789f62180c09b4e54ae5f4b22c5f298c9d
abstractOwl <andrewlo@umail.ucsb.edu>
2013-01-22 23:40:14 -0800

    Finished algorithms

--- src/HashTable.cpp
+++ src/HashTable.cpp
@@ -3,108 +3,59 @@
 #include <string>
 #include "HashTable.h"
 
-// Constructors
-HashTable::HashTable()
-{
-	// Default to 100 index array
-	HashTable(100);
-}
-HashTable::HashTable(int maxSize)
-{
-	entries	=	new Entry[size];
-	size	=	maxSize;
-}
+// Constructor
+HashTable::HashTable() {}
 
 // Private functions
 
-/**
- * Utility function to get hash of string
- */
+// Utility function to get hash of string. Wraps around at 100.
 int HashTable::str2hash(std::string str)
 {
-	return std::hash<std::string>()(str);
+	return std::hash<std::string>()(str) % 100;
 }
-/**
- * Given a key, find its location in the array using binary search
- */
-int HashTable::indexOf(std::string key)
-{
-	return indexOfHelper(str2hash(key), 0, size);
-}
-/**
- * Performs recursion for indexOf()
- */
-int HashTable::indexOfHelper(int hash, int start, int end)
-{
-	int midpoint = (int)floor((start + end) / 2);
-
-	if (start > end) return -1; // hash not found!
-
-	if (entries[midpoint].hash == hash) { // hash found
-		return midpoint;
-	} else if (entries[midpoint].hash > hash) {
-		// midpoint + 1 because we tested midpoint already
-		return indexOfHelper(hash, midpoint + 1, end);
-	} else { // if entries[midpoint].hash < hash
-		// midpoint - 1 because we tested midpoint already
-		return indexOfHelper(hash, start, midpoint - 1);
-	}
-}
-
 // Public functions
-/**
- * Adds a specified string to the HashTable
- */
+
+// Adds a specified string to the HashTable
 void HashTable::insert(std::string key, std::string value)
 {
-	// @@TODO: Fill in function body
-	// REMEMBER TO SET entry->next TO NULL
-	int pos = indexOf(key);
+	int hash	=	str2hash(key);
+
+	// Construct new Entry
+	Entry *newEntry	=	new Entry;
+	newEntry->key	=	key;
+	newEntry->value	=	value;
+	newEntry->hash	=	hash;
+	newEntry->next	=	entries[hash];
+
+	entries[hash]	=	newEntry; // Add new Entry to front
 }
-/**
- * Removes a specified string from the HashTable
- */
+// Removes a specified string from the HashTable
 void HashTable::remove(std::string key)
 {
-	Entry*	curr;
-	int	pos	=	indexOf(key); // Locate key in array
-	
-	if (pos == -1) { // key not found!
-		std::cout << "item not found!";
-	}
-
-	curr = &entries[pos];
+	int		hash	=	str2hash(key);
+	Entry*	curr = entries[hash];
 
 	// Remove Entry containing key from array
-	if (curr->key == key) {
-		entries[pos] = *(curr->next);
+	if (curr->key == key) { // Key is first
+		entries[hash] = curr->next;
 	} else {
-		while (curr != NULL) {
+		while (curr != NULL) { // Search for key
 			if (curr->next->key == key) {
-				curr->next = curr->next->next;
+				curr->next = curr->next->next; // previousKey->next => nextKey
 				break;
 			}
 			curr = curr->next;
 		}
 	}
-
-	// @@TODO: Clean up if arr index is empty
 }
-/**
- * Returns a reference to the instance of specified key in the HashTable if it
- * exists.
- */
+// Returns a reference to the instance of specified key in the HashTable if it
+// exists.
 std::string* HashTable::find(std::string key)
 {
 	Entry*	curr	=	NULL;
-	int		hash	=	str2hash(key),
-			pos;
+	int		hash	=	str2hash(key);
 
-	pos = indexOf(key); // Locate key in array
-	
-	if (pos == -1) return NULL; // key not found!
-
-	curr = &entries[pos];
+	curr = entries[hash];
 
 	// Search for key
 	while (curr != NULL) {
@@ -113,5 +64,6 @@ std::string* HashTable::find(std::string key)
 		}
 		curr = curr->next;
 	}
-	return NULL; // key not found! this shouldn't happen though.
+	return NULL;
 }
+
--- src/HashTable.h
+++ src/HashTable.h
@@ -1,35 +1,26 @@
 #include <string>
 
-/**
- * Entry is one element in the HashTable. Entry contains properties "value",
- * which contains the value of the entry, and "next", which holds a reference
- * to another value with the same hash if there is one.
- */
+// Entry is one element in the HashTable
 typedef struct Entry Entry;
 struct Entry
 {
 	std::string	key;
 	std::string	value;
 	int			hash;
-	Entry*		next;
+	Entry*		next; // Pointer to next item with same hash or NULL
 };
 
-/**
- * HashTable is a data structure containing <key, value> pairs.
- */
+// HashTable is a data structure containing <key, value> pairs.
 class HashTable
 {
 public:
 	HashTable();
-	HashTable(int maxSize);
 	void			insert(std::string key, std::string value);
 	void			remove(std::string key);
 	std::string*	find(std::string key);
 private:
-	Entry	*entries; // Array holding Entry items
-	int		size;
-
-	int	indexOf(std::string key);
-	int	indexOfHelper(int hash, int start, int end);
-	int	str2hash(std::string str);
+	Entry			*entries[100]; // Array holding pointers to Entry items
+	int				size;
+	int				str2hash(std::string str);
 };
+
